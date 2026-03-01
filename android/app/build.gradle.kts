b3fdd5a88a8b9bdfb9450ea3f825b115fdfea798
sahil06sarode <sahil06as@gmail.com>
2026-03-01 20:36:24 +0530

    feat: Initialize Android application build configuration with Flutter and Google Services.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.careai.care_ai"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
